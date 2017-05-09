fb6e71d74f92949ba5270cbbf869ce2ef5e8b6c7
Jason Chang <chihsenc@andrew.cmu.edu>
2017-05-09 11:11:10 -0400

    add about privacy and contact pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,18 @@ class HomeController < ApplicationController
   end
 
   def about
+    @title = "ABOUT"
+    @path_name = "/about"
   end
 
   def contact
+    @title = "CONTACT"
+    @path_name = "/contact"
   end
 
   def privacy
+    @title = "PRIVACY"
+    @path_name = "/privacy"
   end
 
 end
