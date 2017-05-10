e4e3584ec3f4a1fd5821e154c9379dca69b4ce3e
Jason Chang <chihsenc@andrew.cmu.edu>
2017-05-10 01:06:56 -0400

    small changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # set user to new User if not logged in
-    user ||= User.new # i.e., a guest user
+    user ||= User.new
 
-    # set authorizations for different user roles
     if user.role? :admin
-      # they get to do it all
       can :manage, :all
 
     elsif user.role? :manager
