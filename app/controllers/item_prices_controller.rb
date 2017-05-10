189315d010ab768357dd27a524c6bfee30caeb0f
Jason Chang <chihsenc@andrew.cmu.edu>
2017-05-10 08:52:32 -0400

    small changed to item form

--- app/controllers/item_prices_controller.rb
+++ app/controllers/item_prices_controller.rb
@@ -16,6 +16,8 @@ class ItemPricesController < ApplicationController
         format.json { render action: 'show', status: :created, location: @item }
         @manufacturer_price_history = @item.item_prices.manufacturer.chronological.to_a
         @wholesale_price_history = @item.item_prices.wholesale.chronological.to_a
+        display_price = ActionController::Base.helpers.number_to_currency(@item_price.price)
+        flash[:notice] = "#{@item_price.category.capitalize} price has been to updated to #{display_price}."
         format.js
       else
         render action: 'new'
